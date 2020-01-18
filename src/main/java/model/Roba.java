b5a7ea358f31effafc4245a0a7943c4215cdbcdf
milost <milostrosic@gmail.com>
2020-01-18 18:51:26 +0100

    Uradjena klasa roba closes #6

---
+++ src/main/java/model/Roba.java
@@ -0,0 +1,85 @@
+package model;
+
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName="roba")
+public class Roba {
+
+    public static final String POLJE_NAZIV="naziv";
+    public static final String POLJE_OPIS="opis";
+    public static final String POLJE_TEZINA="tezina";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField(columnName = POLJE_NAZIV,canBeNull = false)
+    private String naziv;
+    @DatabaseField(columnName = POLJE_OPIS,canBeNull = false)
+    private int opis;
+    @DatabaseField(columnName = POLJE_TEZINA,canBeNull = false)
+    private double tezina;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true, canBeNull = false)
+    private Avion avion;
+
+    public Roba(String naziv, int opis, double tezina) {
+        this.naziv = naziv;
+        this.opis = opis;
+        this.tezina = tezina;
+    }
+    public Roba() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getOpis() {
+        return opis;
+    }
+
+    public void setOpis(int opis) {
+        this.opis = opis;
+    }
+
+    public double getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(double tezina) {
+        this.tezina = tezina;
+    }
+
+    public Avion getAvion() {
+        return avion;
+    }
+
+    public void setAvion(Avion avion) {
+        this.avion = avion;
+    }
+
+    @Override
+    public String toString() {
+        return "Roba{" +
+                "id=" + id +
+                ", naziv='" + naziv + '\'' +
+                ", opis=" + opis +
+                ", tezina=" + tezina +
+                '}';
+    }
+}
